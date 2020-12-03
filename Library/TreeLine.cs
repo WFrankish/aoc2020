0ed73a705ba0772f6b13f9f821af18617b9c529a
William Frankish <wfrankish@gmail.com>
2020-12-03 21:49:37 +0000

    day 3a - solution

---
+++ Library/TreeLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Library
+{
+    public class TreeLine
+    {
+        private readonly bool[] _trees;
+
+        public TreeLine(string map)
+        {
+            _trees = map.Select(c => c == '#').ToArray();
+        }
+
+        public bool IsTree(int x, Action<string> writeLine)
+        {
+            var index = x % _trees.Length;
+
+            writeLine(string.Join("", _trees.Select((b, i) => i == index
+                ? b ? 'X' : 'O'
+                : b ? '#' : '.'
+            )));
+
+            return _trees[index];
+        }
+    }
+}
---
+++ Solutions/Day3.cs
@@ -0,0 +1,37 @@
+﻿using Inputs;
+using Library;
+using System;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day3 : Day
+    {
+        public Day3(Action<string> writeLine) : base(writeLine, InputHelper.Open(3))
+        {
+        }
+
+        public int PuzzleA()
+        {
+            var trees = InputLines
+                .Select(str => new TreeLine(str))
+                .ToList();
+
+            int collisions = 0;
+            int x = 0;
+            int y = 0;
+            foreach(var treeline in trees)
+            {
+                if(treeline.IsTree(x, WriteLine))
+                {
+                    collisions += 1;
+                }
+
+                x += 3;
+                y += 1;
+            }
+
+            return collisions;
+        }
+    }
+}
