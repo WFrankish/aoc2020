7d568b5078e20e2845dff258f4cfd3b41a452fbc
William Frankish <wfrankish@gmail.com>
2020-12-12 20:53:37 +0000

    day 8a - solution

---
+++ Library/CodeLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Library
+{
+    public class CodeLine
+    {
+        private static readonly Regex _regex = new Regex(@"(\w{3}) (\+|-)(\d+)");
+
+        public string Command { get; }
+
+        public int Value { get; }
+
+        public CodeLine(string line)
+        {
+            var match = _regex.Match(line);
+
+            Command = match.Groups[1].Value;
+
+            Value = int.Parse(match.Groups[3].Value);
+
+            if(match.Groups[2].Value == "-")
+            {
+                Value = -Value;
+            }
+        }
+    }
+}
---
+++ Solutions/Day8.cs
@@ -0,0 +1,54 @@
+﻿using Inputs;
+using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day8 : Day
+    {
+        public Day8(Action<string> writeLine) : base(writeLine, InputHelper.Open(8))
+        {
+        }
+
+        public int PuzzleA()
+        {
+            var lines = InputLines
+                .Select(line => new CodeLine(line))
+                .ToArray();
+
+            var visitedLines = new HashSet<int>();
+
+            var index = 0;
+            var result = 0;
+            while (true)
+            {
+                if (visitedLines.Contains(index))
+                {
+                    break;
+                } else
+                {
+                    visitedLines.Add(index);
+                }
+
+                var codeLine = lines[index];
+                switch (codeLine.Command)
+                {
+                    case "nop":
+                        index++;
+                        break;
+                    case "acc":
+                        result += codeLine.Value;
+                        index++;
+                        break;
+                    case "jmp":
+                        index += codeLine.Value;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
