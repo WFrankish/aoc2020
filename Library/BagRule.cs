6b1c10e4045e22644c64d2bba84ab35d35b70533
William Frankish <wfrankish@gmail.com>
2020-12-12 20:27:57 +0000

    day 7a - solution

---
+++ Library/BagRule.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Library
+{
+    public class BagRule
+    {
+        private static readonly Regex _titleRegex = new Regex(@"^([a-z ]+) bags contain");
+
+        private static readonly Regex _rulesRegex = new Regex(@"(\d+) ([a-z ]+) bags?");
+
+        private readonly Dictionary<string, int> _dict;
+
+        public string Title { get; }
+
+        public BagRule(string line)
+        {
+            var titleMatch = _titleRegex.Match(line);
+
+            Title = titleMatch.Groups[1].Value;
+
+            var rulesMatch = _rulesRegex.Matches(line);
+
+            _dict = rulesMatch.ToDictionary(
+                m => m.Groups[2].Value, 
+                m => int.Parse(m.Groups[1].Value)
+            );
+        }
+
+        public int AllowedBags(string bagType)
+        {
+            return _dict.TryGetValue(bagType, out int value) ? value : 0;
+        }
+    }
+}
---
+++ Solutions/Day7.cs
@@ -0,0 +1,46 @@
+﻿using Inputs;
+using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day7 : Day
+    {
+        public Day7(Action<string> writeLine) : base(writeLine, InputHelper.Open(7))
+        {
+        }
+
+        public int PuzzleA()
+        {
+            var rules = InputLines
+                .Select(line => new BagRule(line))
+                .ToArray();
+
+            var bags = new Stack<string>();
+            bags.Push("shiny gold");
+
+            var foundBags = new HashSet<string>();
+            while (bags.Any())
+            {
+                var bag = bags.Pop();
+                var newBags = rules
+                    .Where(r => r.AllowedBags(bag) > 0)
+                    .Select(r => r.Title)
+                    .ToArray();
+
+                foreach(var newBag in newBags)
+                {
+                    if (!foundBags.Contains(newBag))
+                    {
+                        foundBags.Add(newBag);
+                        bags.Push(newBag);
+                    }                    
+                }
+            }
+
+            return foundBags.Count;
+        }
+    }
+}
